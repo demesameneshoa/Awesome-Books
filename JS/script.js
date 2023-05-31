67bc7c2c0f2b3f74eaef42af5e7d21ef887609b9
demesameneshoa <demesameneshoa@gmail.com>
2023-05-31 20:28:32 +0300

    add menu event listener to diplay dynamic sections

--- JS/script.js
+++ JS/script.js
@@ -83,3 +83,39 @@ function doDate() {
 }
 
 setInterval(doDate, 1000);
+
+// Hide and Display Contents based on menu click
+
+const displayMenu = document.getElementById('nav-display');
+const addMenu = document.getElementById('nav-form');
+const contactMenu = document.getElementById('nav-contact');
+const listSection = document.querySelector('.display');
+const formSection = document.querySelector('.main-form');
+const contactSection = document.querySelector('.contact');
+
+displayMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+  listSection.classList.remove('hidden');
+  formSection.classList.add('hidden');
+  contactSection.classList.add('hidden');
+});
+
+addMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+  listSection.classList.add('hidden');
+  formSection.classList.remove('hidden');
+  contactSection.classList.add('hidden');
+});
+
+contactMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+  listSection.classList.add('hidden');
+  formSection.classList.add('hidden');
+  contactSection.classList.remove('hidden');
+});
+
+window.addEventListener('load', () => {
+  listSection.classList.remove('hidden');
+  formSection.classList.add('hidden');
+  contactSection.classList.add('hidden');
+});
